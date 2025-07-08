9f1743ddd7af35bfb69ab879ae7ece07f8d481be
SergeyFi <fearthunder@yandex.ru>
2025-07-08 22:50:02 +0300

    Add audio pre-processing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from telethon import TelegramClient, events
-from config import API_ID, API_HASH, SESSION_NAME, TARGET_USERS
-from transcriber import transcribe
 import asyncio
 import logging
 import tempfile
 import os
+import subprocess
+from telethon import TelegramClient, events
+from config import API_ID, API_HASH, SESSION_NAME, TARGET_USERS
+from transcriber import transcribe
 
 
 logging.basicConfig(level=logging.INFO)
@@ -24,19 +25,38 @@ async def main():
 
             # Create a temporary file with the extension .ogg
             with tempfile.NamedTemporaryFile(suffix=".ogg", delete=False) as tmp_file:
-                voice_path = tmp_file.name
+                voice_path_ogg = tmp_file.name
 
             # Save voice message
             try:
-                await msg.download_media(file=voice_path)
-                logging.info(f"Voice message saved in: {voice_path}")
+                await msg.download_media(file=voice_path_ogg)
+                logging.info(f"Voice message saved in: {voice_path_ogg}")
             except Exception as e:
                 logging.error(f"Error downloading voice: {e}")
                 return
             
+            # Convert .ogg to .wav
+            with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_wav_file:
+                voice_path_wav = tmp_wav_file.name
+
+            try:
+                subprocess.run(
+                    ["ffmpeg", "-y", "-i", voice_path_ogg, "-ar", "16000", "-ac", "1", voice_path_wav],
+                    check=True,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+                logging.info(f"Converted to WAV: {voice_path_wav}")
+                
+            except subprocess.CalledProcessError as e:
+                logging.error(f"ffmpeg failed: {e}")
+                return
+            finally:
+                os.remove(voice_path_ogg)
+            
             # Decrypting and sending the message
             try:
-                text = transcribe(voice_path)
+                text = transcribe(voice_path_wav)
                 logging.info(f"Transcript: {text}")
 
                 await client.send_message(entity=sender_name, message=text, reply_to=msg.id)
@@ -44,7 +64,7 @@ async def main():
             except Exception as e:
                 logging.error(f"Decryption error: {e}")
             finally:
-                os.remove(voice_path)
+                os.remove(voice_path_wav)
 
     logging.info("Waiting for incoming messages...")
     await client.run_until_disconnected()
--- transcriber.py
+++ transcriber.py
@@ -1,14 +1,26 @@
 import logging
-from faster_whisper import WhisperModel
-from config import MODEL_SIZE
 import time
+import os
+import tempfile
+import subprocess
+from faster_whisper import WhisperModel, BatchedInferencePipeline
+from config import MODEL_SIZE
 
 model = WhisperModel(MODEL_SIZE, device="cpu", compute_type="int8")
+batched_model = BatchedInferencePipeline(model=model)
 
 logging.basicConfig(level=logging.INFO)
 
 def transcribe(audio_path: str) -> str:
-    segments, info = model.transcribe(audio_path, beam_size=5, language="ru", vad_filter=True, temperature=0.2)
+    clean_audio_path = denoise_audio(audio_path)
+
+    segments, info = batched_model.transcribe(
+        clean_audio_path, 
+        beam_size=5,
+        language="ru",
+        vad_filter=True,
+        temperature=0.1,
+        )
     
     start_time = time.time()
 
@@ -23,3 +35,20 @@ def transcribe(audio_path: str) -> str:
     logging.info(f"Transcription: {elapsed_time:.2f} sec, audio duration: {info.duration:.2f} sec, speed: {speed_ratio:.2f}x")
 
     return text
+
+
+def denoise_audio(input_path: str) -> str:
+    tmp_fd, clean_path = tempfile.mkstemp(suffix=".wav")
+    os.close(tmp_fd)
+    try:
+        subprocess.run([
+            "sox", input_path, clean_path,
+            "highpass", "100",
+            "lowpass", "4000",
+            "norm",
+            "gain", "-n"
+        ], check=True)
+        return clean_path
+    except subprocess.CalledProcessError as e:
+        logging.warning(f"Error clearing audio: {e}")
+        return input_path
